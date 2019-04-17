db1bdafc170611cd3470658c5a00200396e72976
PTMcEneaney <pagemceneaney@gmail.com>
2019-04-17 11:04:42 -0700

    create cards via a function in the js file instead writing them individually

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,73 +25,101 @@ if defender HP is 0 or less and other defenders are greater than 0
     allows you to select the next defender 
 */
 $(document).ready(function() {
+var activeHP = undefined;
+var pcAttack = undefined;
+var decenderHP = undefined;
+var defenderAttack = undefined;
 
 
-$('.character').on("click", function() {
-    if ($('.enemies').is(':empty')) {
-        $(this).addClass('active');
-        $(this).removeClass('character');
-        $('.yourCharacter').append($('.active'));
-        $('.enemies').append($('.character'));
-    } else if ($('.activeDefender').is(':empty')) {
-        $(this).addClass('defender');
-        $('.defender').appendTo($('.activeDefender'));
-    } else {
-
-    }
-
-});
-
-var characters = [
+var fighter = [
 johnMcclane = {
     name: "John McClane",
     image: "assets/images/johnMcclane3.jpg",
     startingHP: 150,
-    playerAttack: 7,
+    pcAttack: 7,
     defenderAttack: 25,
+    status: undefined,
 },
 hansGruber = {
     name: "Hans Gruber",
     image: "assets/images/hansGruber3.jpg",
     startingHP: 180,
-    attack: 5,
+    pcAttack: 5,
     defenderAttack: 25,
+    status: undefined,
 },
 alPowell = {
     name: "Sgt. Al Powell",
     image: "assets/images/alPowell3.jpg",
     startingHP: 120,
-    attack: 9,
+    pcAttack: 9,
     defenderAttack: 25,
+    status: undefined,
+
 },
 toniVreski = {
     name: "Toni Vreski",
     image: "assets/images/tonyVreski3.png",
     startingHP: 100,
-    attack: 11,
+    pcAttack: 11,
     defenderAttack: 25,
+    status: undefined,
+
 }];
 
-$(".fightBtn").on("click", function () {
+var cardInit = 0;
 
-});
-
-/* var cards = function(character) {
-    var newCard = $('<div></div>').addClass('card btn character').attr("style", 'width: 17rem;');
+var cards = function(character) {
+    var newCard = $('<div></div>').addClass('card btn character').attr("style", 'width: 17rem;').attr("fighterNum", cardInit);
     var name = $('<h5></<h5>').text(character.name).addClass('card-title');
     var image = $('<img/>').attr("src", character.image).addClass('card-img-top');
     var body = $('<div></div>').addClass('card-body');
     var text = $('<p></p>').text(character.startingHP + " HP").addClass('card-text');
     $('.mainSelection').append(newCard);
+    $(`[fighterNum=${cardInit}]`).append(name, image, body, text);
+    // $('[fighterNum=cardInit]')
 
-    if ($('.btn').is(':empty')) {
-        $('.btn').append(name, image, body, text);
-    }
+    cardInit++;
+
 };
-cards(johnMcclane);
-cards(toniVreski); */
-// cards(alPowell);
-// cards(hansGruber);
+for (var i = 0; i < fighter.length; i++) {
+    cards(fighter[i]);
+}
+
+$('.character').on("click", function() {
+    if ($('.enemies').is(':empty')) {
+        $(this).addClass('active');
+        var i = this.getAttribute("fighterNum");
+        activeHP = fighter[i].startingHP;
+        pcAttack = fighter[i].pcAttack;
+        fighter[i].status = "active";
+        console.log(i);
+
+        $(this).removeClass('character');
+        $('.yourCharacter').append($('.active'));
+        $('.enemies').append($('.character'));
+        //assigning the clicked card to pre-defined variables
+        
+    } else if ($('.activeDefender').is(':empty')) {
+        $(this).addClass('defender');
+        $('.defender').appendTo($('.activeDefender'));
+        var j = this.getAttribute("fighterNum");
+
+        decenderHP = fighter[j].startingHP;
+        defenderAttack = fighter[j].defenderAttack;
+        fighter[j].status = "defender";
+        console.log(j);
+
+        // console.log($('.defender').getAttribute("fighterNum"));
+    }
+
+});
+
+$(".fightBtn").on("click", function () {
+   
+});
+
+
 
     
 // } else if ($('.enemies').is(":empty"))  { 
